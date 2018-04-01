e71c79b84e1bfd86197c94ae7b93e367a7d59212
LeonhardDavid <34747852+LeonhardDavid@users.noreply.github.com>
2018-04-01 12:14:16 +0300

    Initial commit

---
+++ header_footer/header.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Webpage </title>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/colors.css" type="text/css"/>
+    <link rel="stylesheet" href="css/styles.css" type="text/css"/>
+    <link rel="stylesheet" href="css/menu.css" type="text/css"/>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <!-- Emoji CSS -->
+    <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+
+
+  </head>
+
+  <body style="background-color: #cccccc;">
+
+  <nav class="navbar navbar-expand-lg navbar-custom">
+  
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"> </a>
+
+  <div class="collapse navbar-collapse justify-content-between" id="collapsibleNavbar">
+    <ul class="navbar-nav mr-aut">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><b> Informatii e-commerce: 0740 104 194 </b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Categorii</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Produse</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Parteneri</a>
+      </li>  
+    </ul>
+    <ul class="navbar-nav">
+      <li>
+        <a class="nav-link" href="#"> Bine ai venit, Nume cont!</a>
+      </li>
+      <li>
+        <a class="nav-link" href="#"> <i class="fas fa-user"></i> Logout</a>
+      </li>
+      <li>
+        <div class="dropdown">
+           <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> <i class="em em-flag-ro"></i> RO</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#"><i class="em em-gb"></i> EN</a>
+            <a class="dropdown-item" href="#"><i class="em em-de"></i> DE</a>
+            <a class="dropdown-item" href="#"><i class="em em-fr"></i> FR</a>
+          </div>
+        </div>
+        
+      </li>
+    </ul>
+  </div>  
+</nav>
+
+    <!--
+    <div class="top_line"> &nbsp; </div>
+
+    <div class="custom_nav">
+        <div class="custom_nav_div">
+          <a href="#" class="custom_nav_a"> <b>Informatii e-commerce: 0740 104 194 </b> </a>
+        </div>
+        <div class="custom_nav_div">
+          <a href="#" class="custom_nav_a"> Categorii </a>
+        </div>
+        <div class="custom_nav_div">
+          <a href="#" class="custom_nav_a"> Produse </a>
+        </div>
+        <div class="custom_nav_div">
+          <a href="#" class="custom_nav_a"> Parteneri </a>
+        </div>
+
+        <div class="custom_nav_div_right">
+          <a href="#" class="custom_nav_a"> <i class="fas fa-user"></i> Logout </a>
+        </div>
+
+        <div class="custom_nav_div_right">
+          <a href="#" class="custom_nav_a"> Bine ai venit, <i>Nume cont</i>! </a>
+        </div>
+
+        <div class="custom_nav_div_right_dropdown">
+          <a href="#" class="custom_nav_a"> <i class="em em-flag-ro"></i> RO <i class="fas fa-angle-down"></i> </a>
+
+          <div class="custom_nav_div_right_dropdown-content">
+            
+            <div class="custom_nav_dropdown_elements">            
+              <a href="#" class="custom_nav_a"> <i class="em em-gb"></i> EN </a>
+            </div>
+
+            <div class="custom_nav_dropdown_elements">
+              <a href="#" class="custom_nav_a"> <i class="em em-de"></i> DE </a>
+            </div>
+            
+            <div class="custom_nav_dropdown_elements">
+              <a href="#" class="custom_nav_a"> <i class="em em-fr"></i> FR </a>
+            </div>
+          </div>
+
+        </div>
+    </div>
+    -->
+
