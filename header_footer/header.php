a270e18c06abf983549fa989c17a652c3616d897
LeonhardDavid <34747852+LeonhardDavid@users.noreply.github.com>
2018-04-02 13:18:43 +0300

    NEW

---
+++ header_footer/header.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Webpage </title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/menu.css" type="text/css"/>
+    <link rel="stylesheet" href="css/styles.css" type="text/css"/>
+    <link rel="stylesheet" href="css/colors.css" type="text/css"/>
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Emoji CSS -->
+    <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+
+
+  </head>
+
+  <body style="background-color: #cccccc;">
+
+  <nav class="navbar navbar-expand-lg navbar-custom" style="z-index:9999">
+
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"> </a>
+
+  <div class="collapse navbar-collapse justify-content-between" id="collapsibleNavbar">
+    <ul class="navbar-nav mr-aut">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><b> Informatii e-commerce: 0740 104 194 </b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Categorii</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Produse</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Parteneri</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+      <li>
+        <a class="nav-link" href="#"> Bine ai venit, Nume cont!</a>
+      </li>
+      <li>
+        <a class="nav-link" href="#"> <i class="fas fa-user"></i> Logout</a>
+      </li>
+      <li>
+        <div class="dropdown">
+           <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> <i class="em em-flag-ro"></i> RO</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#"><i class="em em-gb"></i> EN</a>
+            <a class="dropdown-item" href="#"><i class="em em-de"></i> DE</a>
+            <a class="dropdown-item" href="#"><i class="em em-fr"></i> FR</a>
+          </div>
+        </div>
+
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container d-none d-lg-block" style="z-index:9999;position:relative;">
+
+  <div class="row bg form-group">
+
+    <div class="col-md-2 d-flex">
+      <img class="mx-auto my-auto" src="images/logo.svg" alt ="Logo123" style="height:4.5rem">
+    </div>
+
+    <div class="col-md-8 my-1">
+        <form>
+          <div class="form-group my-3 mx-4" >
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Cauta produsul dorit...">
+                  <span class="input-group-btn">
+                      <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                  </span>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="col-md-2 d-flex justify-content-center align-self-center" style="background-color:pink">
+        <div class="d-flex align-self-center padds">
+          <i class="fas fa-users colorcusti"></i>
+          <span style="pstyle">
+           Contul
+           <br>
+           meu
+         </span>
+        </div>
+        <div class="d-flex align-self-center padds">
+          <i class="fas fa-cart-arrow-down colorcusti"></i>
+          <span style="pstyle">
+           Cosul
+           <br>
+           meu
+         </span>
+        </div>
+      </div>
+    </div>
+    <div class="row rounded-bottom">
+      <div class="col-sm-3 rounded-bottom">
+        <div class="dropdown">
+          <div class="dropdown-toggle rounded-bottom custtxt" data-toggle="dropdown">
+                Categorii produse
+          </div>
+          <div class="dropdown-menu" style="width:410%">
+            <div class="row">
+                <div class="col-4">
+                  <div class="list-group" id="list-tab" role="tablist" style="width:60%">
+                    <a class="list-group-item list-group-item-action active dropdown-item" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">ADIDAS ORIGINALS</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">NIKELAB</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">SPORTS</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">ATHLETES</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-home1-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">MEN'S SHOES</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-profile1-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">MEN'S CLOTHING</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-messages1-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">WOMEN'S SHOES</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-settings2-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">WOMEN'S CLOTHING</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-settings3-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">KID'S SHOES</a>
+                    <a class="list-group-item list-group-item-action dropdown-item" id="list-settings4-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">KID'S CLOTHING</a>
+                  </div>
+                </div>
+                <div class="col-8">
+                  <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active " id="list-home" role="tabpanel" aria-labelledby="list-home-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-home" role="tabpanel" aria-labelledby="list-home1-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile1-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages1-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings2-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings3-list"> Lorem Ipsum</div>
+                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings4-list"> Lorem Ipsum</div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+  <div class="col-sm-9" style="background-color: white;">
+    PROMOTII / PRETURI SPECIALE / NOUTATI / BRANDURI
+  </div>
+  </div>
+  </div>
+  <script type="text/javascript" src="../js/header.js">
+  </script>
---
+++ index.php
@@ -0,0 +1,220 @@
+<?php require 'header_footer/header.php';?>
+
+<!-- Begin code -->
+
+	<!-- First photo slider -->
+	<div id="carouselExampleIndicators-main1" class="carousel slide translate-y-up" data-ride="carousel" data-interval="false" data-keyboard="true" >
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators-main1" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators-main1" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators-main1" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100 main-slider" src="images/image1.jpg" alt="First slide" >
+	      	<div class="carousel-caption d-none d-md-block">
+			    <h5>First slide label</h5>
+			    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+			</div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100 main-slider" src="images/image2.jpg" alt="Second slide" >
+		    <div class="carousel-caption d-none d-md-block">
+				    <h5>Second slide label</h5>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+			</div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100 main-slider" src="images/image3.jpg" alt="Third slide" >
+	      	<div class="carousel-caption d-none d-md-block">
+			    <h5>Third slide label</h5>
+			    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+			</div>
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators-main1" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators-main1" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+	<!-- Top menu -->
+
+
+	<!-- Middle Bar -->
+
+	<div class="container translate-y-up colorcust pad">
+		<div class="d-flex justify-content-center align-content-center">
+			<div class="row w-100">
+		  		<div class="col-md-3"><i class="far fa-credit-card"></i> Raport pret calitate impecabil</div>
+		  		<div class="col-md-3"> <i class="fas fa-fighter-jet"></i> Produse originale New York, SUA </div>
+		  		<div class="col-md-3"> <i class="fas fa-hand-holding-usd"></i> Retur fara explicatii in 15 zile</div>
+		  		<div class="col-md-3"><i class="fas fa-shipping-fast"></i> Livrare gratuita de la 290 lei</div>
+			</div>
+		</div>
+
+		<!-- Product slider with category and images -->
+
+		<div class="container d-none d-sm-block colorcust pad2">
+			<div class="row">
+			  <div class="col-sm-4 ">
+			    <div class="list-group" id="list-tab" role="tablist">
+			      <a class="list-group-item list-group-item-action blue-bg" id="list-item1-list" data-toggle="list" href="#list-item1" role="tab" aria-controls="item1">CATEGORII</a>
+			      <a class="list-group-item list-group-item-action" id="list-item1-list" data-toggle="list" href="#list-item1" role="tab" aria-controls="item1">SPORTURI ACVATICE</a>
+			      <a class="list-group-item list-group-item-action" id="list-item2-list" data-toggle="list" href="#list-item2" role="tab" aria-controls="item2">SPORTURI DE IARNA</a>
+			      <a class="list-group-item list-group-item-action" id="list-item3-list" data-toggle="list" href="#list-item3" role="tab" aria-controls="item3">FITNESS & CARDIO</a>
+			      <a class="list-group-item list-group-item-action" id="list-item4-list" data-toggle="list" href="#list-item4" role="tab" aria-controls="item4">FOTBAL & BASKET</a>
+			      <a class="list-group-item list-group-item-action" id="list-item5-list" data-toggle="list" href="#list-item5" role="tab" aria-controls="item5">INCALTAMINTE</a>
+			      <a class="list-group-item list-group-item-action" id="list-item6-list" data-toggle="list" href="#list-item6" role="tab" aria-controls="item6">GECI & JACHETE</a>
+			      <a class="list-group-item list-group-item-action" id="list-item7-list" data-toggle="list" href="#list-item7" role="tab" aria-controls="item7">SKATES & ROLLERS</a>
+			      <a class="list-group-item list-group-item-action" id="list-item8-list" data-toggle="list" href="#list-item8" role="tab" aria-controls="item8">ACCESORII CAMPING</a>
+			      <a class="list-group-item list-group-item-action" id="list-item9-list" data-toggle="list" href="#list-item9" role="tab" aria-controls="item9">DRUMETIE & TIMP LIBER</a>
+			    </div>
+			  </div>
+			  <div class="col-sm-8">
+			    <div class="tab-content" id="nav-tabContent">
+			      <div class="tab-pane fade" id="list-item1" role="tabpanel" aria-labelledby="list-item1-list">
+
+			      	<div class="row">
+			      		<div class="col-sm-6">
+
+			      			<!-- Mini-slider 1-->
+								<div id="carouselExampleIndicators-item1" class="carousel slide" data-ride="carousel" data-interval="false" data-keyboard="true">
+									<ol class="carousel-indicators">
+								    	<li data-target="#carouselExampleIndicators-item1" data-slide-to="0" class="active"></li>
+								    	<li data-target="#carouselExampleIndicators-item1" data-slide-to="1"></li>
+								    	<li data-target="#carouselExampleIndicators-item1" data-slide-to="2"></li>
+								  	</ol>
+								  <div class="carousel-inner">
+								    <div class="carousel-item active">
+
+								      <img class="d-block w-100 mini-slider" src="images/image1.jpg" alt="First slide" >
+
+								    </div>
+								    <div class="carousel-item">
+
+								      <img class="d-block w-100 mini-slider" src="images/image2.jpg" alt="Second slide" >
+
+								    </div>
+								    <div class="carousel-item">
+
+								      <img class="d-block w-100 mini-slider" src="images/image3.jpg" alt="Third slide" >
+
+								    </div>
+								  </div>
+								</div>
+			      			</div>
+
+				      	<div class="col-sm-6">
+				      		<!-- Extra Images 1 -->
+				      		<div class="row" >
+				      			<img class="d-block w-100 h-50 extra" src="images/image1.jpg" alt="extra-image1" >
+				      		</div>
+
+				      		<!-- Extra Images 2 -->
+				      		<div class="row">
+				      			<div class="col-sm-6">
+				      				<div class="row">
+				      					<img class="d-block w-100 h-100 extra-2" src="images/image-vertical-1.jpg" alt="extra-image2" >
+				      				</div>
+				      			</div>
+				      			<div class="col-sm-6">
+				      				<div class="row">
+				      					<img class="d-block w-100 h-100 extra-2" src="images/image-vertical-1.jpg" alt="extra-image3" >
+				      				</div>
+				      			</div>
+
+				      		</div>
+				      	</div>
+			      	</div>
+
+			      </div>
+
+			      <div class="tab-pane fade" id="list-item2" role="tabpanel" aria-labelledby="list-item2-list">
+			      	item2
+			      </div>
+
+			      <div class="tab-pane fade" id="list-item3" role="tabpanel" aria-labelledby="list-item3-list">
+			      	list-item3
+			      </div>
+
+			      <div class="tab-pane fade" id="list-item4" role="tabpanel" aria-labelledby="list-item4-list">
+			      	item4
+			      </div>
+
+			      <div class="tab-pane fade" id="list-item5" role="tabpanel" aria-labelledby="list-item5-list">
+			      	item5
+			      </div>
+
+			      <div class="tab-pane fade" id="list-item6" role="tabpanel" aria-labelledby="list-item6-list">
+			      	item6
+			      </div>
+
+			      <div class="tab-pane fade" id="list-item7" role="tabpanel" aria-labelledby="list-item7-list">
+			      	item7
+			      </div>
+
+			      <div class="tab-pane fade" id="list-item8" role="tabpanel" aria-labelledby="list-item8-list">
+			      	item8
+			      </div>
+
+			      <div class="tab-pane fade" id="list-item9" role="tabpanel" aria-labelledby="list-item9-list">
+			      	item9
+			      </div>
+
+			    </div>
+			  </div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- Second photo slider-->
+
+	<div id="carouselExampleIndicators-main2" class="carousel slide translate-y-up" data-ride="carousel" data-interval="false" data-keyboard="true" >
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators-main2" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators-main2" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators-main2" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100 main-slider" src="images/image4.jpg" alt="First slide" >
+	      	<div class="carousel-caption d-none d-md-block">
+			    <h5>First slide label</h5>
+			    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+			</div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100 main-slider" src="images/image5.jpg" alt="Second slide" >
+		    <div class="carousel-caption d-none d-md-block">
+				    <h5>Second slide label</h5>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+			</div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100 main-slider" src="images/image6.jpg" alt="Third slide" >
+	      	<div class="carousel-caption d-none d-md-block">
+			    <h5>Third slide label</h5>
+			    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+			</div>
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators-main2" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators-main2" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+<!-- End code -->
+
+<?php require 'header_footer/footer.php';?>
