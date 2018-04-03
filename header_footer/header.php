8b217a83226c07e816d1ca492f7daea7d8aa62dc
KillerBananaZ <KillerBananaZ@users.noreply.github.com>
2018-04-03 16:27:56 +0300

    push2

--- header_footer/header.php
+++ header_footer/header.php
@@ -7,10 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/colors.css" type="text/css"/>
-    <link rel="stylesheet" href="css/menu.css" type="text/css"/>
     <link rel="stylesheet" href="css/styles.css" type="text/css"/>
-
+    <link rel="stylesheet" href="css/menu.css" type="text/css"/>
+    <link rel="stylesheet" href="css/colors.css" type="text/css"/>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -25,7 +24,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-custom" style="z-index:9999">
 
-    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand" href="#"> </a>
@@ -67,9 +66,10 @@
   </div>
 </nav>
 
+
 <div class="container d-none d-lg-block" style="z-index:9999;position:relative;">
 
-  <div class="row bg form-group">
+  <div class="row bg">
 
     <div class="col-md-2 d-flex">
       <img class="mx-auto my-auto" src="images/logo.svg" alt ="Logo123" style="height:4.5rem">
@@ -77,7 +77,7 @@
 
     <div class="col-md-8 my-1">
         <form>
-          <div class="form-group my-3 mx-4" >
+          <div class="form-group my-3 mx-4">
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Cauta produsul dorit...">
                   <span class="input-group-btn">
@@ -88,69 +88,36 @@
         </form>
       </div>
 
-      <div class="col-md-2 d-flex justify-content-center align-self-center" style="background-color:pink">
-        <div class="d-flex align-self-center padds">
-          <i class="fas fa-users colorcusti"></i>
-          <span style="pstyle">
-           Contul
-           <br>
-           meu
-         </span>
-        </div>
-        <div class="d-flex align-self-center padds">
-          <i class="fas fa-cart-arrow-down colorcusti"></i>
-          <span style="pstyle">
-           Cosul
-           <br>
-           meu
-         </span>
-        </div>
+      <div class="col-md-1 d-flex align-self-center justify-content-center" style="background-color:pink">
+        <a href="#"><i class="fas fa-shopping-cart"></i>Contul meu</a>
+
+      </div>
+
+      <div class="col-md-1 d-flex justify-content-center align-self-center" style="background-color:lightgrey">
+        <i class="fas fa-shopping-cart"></i> Cosul meu
       </div>
     </div>
-    <div class="row rounded-bottom">
-      <div class="col-sm-3 rounded-bottom">
-        <div class="dropdown">
-          <div class="dropdown-toggle rounded-bottom custtxt" data-toggle="dropdown">
-                Categorii produse
-          </div>
-          <div class="dropdown-menu" style="width:410%">
-            <div class="row">
-                <div class="col-4">
-                  <div class="list-group" id="list-tab" role="tablist" style="width:60%">
-                    <a class="list-group-item list-group-item-action active dropdown-item" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">ADIDAS ORIGINALS</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">NIKELAB</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">SPORTS</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">ATHLETES</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-home1-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">MEN'S SHOES</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-profile1-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">MEN'S CLOTHING</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-messages1-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">WOMEN'S SHOES</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-settings2-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">WOMEN'S CLOTHING</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-settings3-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">KID'S SHOES</a>
-                    <a class="list-group-item list-group-item-action dropdown-item" id="list-settings4-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">KID'S CLOTHING</a>
-                  </div>
-                </div>
-                <div class="col-8">
-                  <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active " id="list-home" role="tabpanel" aria-labelledby="list-home-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-home" role="tabpanel" aria-labelledby="list-home1-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile1-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages1-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings2-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings3-list"> Lorem Ipsum</div>
-                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings4-list"> Lorem Ipsum</div>
-                  </div>
-                </div>
-              </div>
-          </div>
-        </div>
+
+    <div class="row">
+      <div class="col-md-3 n1 d-flex justify-content-between hvr">
+            CATALOG PRODUSE
+            <span>
+              <i class="fas fa-align-justify"></i>
+            </span>
+
       </div>
-  <div class="col-sm-9" style="background-color: white;">
-    PROMOTII / PRETURI SPECIALE / NOUTATI / BRANDURI
+
+      <div class="col-md-9 n2">
+           <a href="#">PROMOTII</a>
+           <span class="slash">/</span>
+           <a href="#">PRETURI SPECIALE</a>
+           <span class="slash">/</span>
+           <a href="#">NOUTATI</a>
+           <span class="slash">/</span>
+           <a href="#">BRANDURI</a>
+           <a href="#" class="float-right"> <i class="fas fa-info-circle"></i>&nbsp;INFOCENTER</a>
+      </div>
   </div>
+
   </div>
-  </div>
-  <script type="text/javascript" src="../js/header.js">
   </script>
