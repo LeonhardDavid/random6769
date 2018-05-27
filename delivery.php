21e1d82d9df305624595d60205bd3893ca74cca2
KillerBananaZ <KillerBananaZ@users.noreply.github.com>
2018-05-27 16:29:42 +0300

    Delivery.php stilizat asa blana cum ne place noua

---
+++ delivery.php
@@ -0,0 +1,103 @@
+<?php require 'header_footer/header.php';?>
+<div class="container white-bg px-4">
+  <div class="content-extended">
+    <div class="row">
+      <div class="col-md-12 my-4">
+        <h5 class="font-weight-bold">Livrare</h5>
+    <p>  ▸Produsele se livreaza cu DPD sau Prioripost in termen de 24-48 de ore. In cazuri exceptionale termenul de livrare poate depasi 48 de ore, caz in care clientul va fi contactat.
+
+    <p>  ▸Plata se face in momentul in care primiti coletul (ramburs) sau online cu cardul.
+
+    <p> <b> ATENŢIE: </b> Comenzile pot fi livrate însă şi în două sau trei zile lucrătoare, din diferite motive, independente de https://victoriavynn.ro : condiţii meteo, probleme tehnice ale companiei de curierat, concedii, accesul greu al localităţii de livrare.
+
+    <p>  ▸Clienţii vor fi anunţaţi în prealabil de întârziere, în cazul în care această apare.
+
+    <p> <b> ▸Se livrează în 24 h (ziua următoare), doar comenzile plasate până în ora 15:30 a unei zile lucrătoare!</b> După ora 15:30 coletele sunt preluate de compania de curierat, urmând să ajungă în posesia dvs în următoarele 2 zile lucrătoare.
+
+    <p>  ▸Exemplu de Livrări:
+
+    <p>  a) Comezile plasate în zilele: Luni, Marţi, Miercuri, Joi până în ora 15:30- se vor livra a doua zi
+
+    <p>  b) Comenzile plasate în zilele: Vineri după ora 15:00 - se vor livra Luni
+
+    <p>  c) Comenzile plasate în zilele: Sâmbătă şi Duminică - se vor livra Luni
+
+    <p>  ▸În cazul în care pe site sunt înregistrate foarte multe comenzi, de obicei după zilele de weekend, ne rezervăm dreptul de a mări perioada maximă de livrare cu 1-2 zile lucrătoare. Costurile transportului sunt:
+
+    <p>  -19,15 lei pentru comenzi de pana in 290 de lei;
+
+    <p>  Pentru comenzi de minim 290 lei, LIVRAREA ESTE GRATUITA!
+
+      Nu ezitati sa ne contactati daca aveti nelamuriri.
+
+      <h5 class="font-weight-bold">Retur</h5>
+    <p>  ▸ Stimați Clienti, în cazul în care <b>doriți să returnați </b> orice produs cumpărat de pe site-ul https://victoriavynn.ro , compania noastră va trimite un curier din partea firmei de curierat DPD pentru a prelua coletul dumneavoastră.
+
+    <p>  ▸ Banii dumneavoastră vor fi restituiţi <b> conform pct 1. de mai jos, în maximum 14 zile calendaristice.</b>
+
+      <p>▸ De asemenea, trebuie să ne înştiinţați în maximum 14 zile calendaristice de intenţia dumneavoastră de retur, conform <b>legii OUG 34 din 13 Iunie.</b>
+
+      <h5 class="font-weight-bold">Întrebări și răspunsuri utile:</h5>
+
+    <p class="font-weight-bold">  1. Cine plăteşte returul?</p>
+
+    <p>  ▸ Clientul plătește transportul returului,</p>
+
+    <p>  <b>▸ Magazinul este obligat să restituie suma în maxim 14 zile calendaristice </b>, iar suma trebuie să includă și costul livrării inițiale, cu excepția cazului în care clientul a cerut o livrare mai rapidă decât cea standard.</p>
+
+    <p>  ▸ Articol din Legea OUG nr.34 din 13 iunie 2014: <span class="font-italic">(1) Profesionistul rambursează toate sumele pe care le-a primit drept plată din partea consumatorului, inclusiv, după caz, costurile livrării.(Articolul 13 Obligaţiile care revin profesionistului în cazul retragerii, alin 1)</span></p>
+
+    <p class="font-weight-bold">  2. Produsul trebuie returnat în ambalajul original?</p>
+
+    <p>  <b>▸ Legea OUG nr.34 din 13 iunie 2014 NU obligă</b>clientul să returneze produsul în ambalajul original, însă noi vă recomandăm să faceţi pe cât posibil acest lucru, întrucât produsul să nu fie deteriorat pe parcursul transportării.</p>
+
+    <p>  ▸ Avem dreptul de a nu accepta produsele care au o stare vizibilă deteriorată, indiferent de motivele care au dus la acest aspect.</p>
+
+    <p class="font-italic">  ▸ Articol din Legea OUG nr.34 din 13 iunie 2014: Consumatorul este responsabil doar în ceea ce priveşte diminuarea valorii produselor rezultată din manipularea acestora, diferit de ceea ce este necesar pentru determinarea naturii, caracteristicilor şi funcţionării produselor. Diminuarea valorii produselor nu trebuie să fie descurajantă pentru consumator în a-şi exercita dreptul de retragere.</p>
+
+    <p class="font-weight-bold">  3. În câte zile pot returna produsul?</p>
+
+    <p>  <b>▸ Articolul 24 din Legea OUG nr.34</b>din 13 iunie 2014 indică faptul că un client poate returna produsul<b> în termen de 14 zile calendaristice</b> de la data primirii produsului, în cazul contractelor de vânzare.</p>
+
+    <p class="font-italic">  ▸ Legea OUG nr.34 din 13 iunie 2014 : Dacă un termen exprimat în zile se calculează din momentul în care intervine un eveniment sau se realizează un act, ziua în cursul căreia a avut loc acest eveniment sau se realizează acest act nu se ia în seamă la calculul termenului, cu excepţia cazului în care prezenta ordonanţă de urgenţă prevede altfel. (3) În cazul în care ultima zi a termenului este o zi nelucrătoare, profesionistul prelungeşte termenul în mod corespunzător</p>
+
+    <p class="font-weight-bold">  4. Pentru ce motiv se poate returna un produs achizitonat?</p>
+
+    <p> <b> ▸ Articolul 4 din Legea OUG nr.34 </b> indică faptul că produsul <b> poate fi returnat fără un motiv specific </b> în termen de 14 zile calendaristice de la data primirii produsului.</p>
+
+    <p class="font-weight-bold">  5. Cum returnez un produs?</p>
+
+    <p>  ▸ Modalitate de returnare:</p>
+
+    <p class="font-weight-bold">  E-mail pentru retur: victoriavynnromania@gmail.com sau pe pagina de Contact</p>
+
+    <p>  ▸ Vă rugăm să ne înştiinţați de intenţia de a returna produsul printr-un e-mail care să conţină datele următoare:</p>
+
+    <p>  numele pe care s-a făcut comanda,numărul comenzii, produsul pe care doreşti să-l returnezi şi codul acestuia (găseşti aceste detalii în mesajele de confirmare primite după efectuarea comenzii)</p>
+
+    <p class="font-weight-bold font-italic">  În cazul în care nu există oferta returului gratuit din partea firmei noastre, trebuie urmate urmatoarele instrucțiuni:</p>
+
+    <p>  ▸ Vă rugăm să ne înștiințați prin email (victoriavynnromania@gmail.com) de intenția dumneavoastră de retur</p>
+
+    <p>  ▸ După confirmarea de primire a solicitării din partea noastră, ne puteți returna produsul EXCLUSIV prin curier rapid, cu menţiunea ca pe colet să fie menţionat numele pe care s-a plasat comanda</p>
+
+    <p>  ▸ De îndată ce noi primim produsul și il vom verifica îţi vom returna banii pentru produsul returnat (în maximum 14 zile calendaristice).</p>
+
+    <p>  ▸ Pentru retur, singurul cost pe care îl vei suporta va fi costul de transport al produsului către sediul nostru, fără a exista alte taxe suplimentare.</p>
+
+    <p>  <b>Adresa de Retur: </b> Targu Jiu, Str. Nicolae Titulescu Bl.10, Sc.1, Ap.4</p>
+
+    <p class="font-weight-bold">  Exemplu Date Înscrise pe Colet Retur:</p>
+
+    <p>  Destinatar: S.C VAPEPOINT 84 SRL SRL
+      Adresă: Targu Jiu, Str. Nicolae Titulescu Bl.10, Sc.1, Ap.4
+      Telefon: 0765.428.648</p>
+
+    <p>  Email : victoriavynnromania@gmail.com</p>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+<?php require 'header_footer/footer.php';?>
